db8f2b8d940e5ac7ca882a0151c01bed63b3292d
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2026-06-20 21:34:55 -0500

    Chapter5 - finished init cat example

--- chapters/ch5/cat_example/main.go
+++ chapters/ch5/cat_example/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
+	"unsafe"
 )
 
 /*
 This example demonstrate how to implement the simple cat but in a better idiomatic way, with better practices
 */
+
+const bufferFileSize = 2048
+
 func main() {
 
 	args := os.Args
@@ -17,9 +22,37 @@ func main() {
 
 	file, closer, err := getFile(args[1])
 
+	if err != nil {
+		log.Fatalf("An error occurred open the file. file ={%s}", args[1])
+	}
+
+	defer closer()
+
+	bufferFile := make([]byte, bufferFileSize)
+	for {
+		count, err := file.Read(bufferFile)
+		if count == 0 {
+			//log.Printf("count = {%d}, bufferFile = {%s}, EOF.", count, string(bufferFile))
+			//log.Printf("count = {%d}, bufferFile = {%s}, EOF.", count, string(bufferFile))
+			//zero copy  conversion high performance byte[] to string
+			str := unsafe.String(&bufferFile[0], len(bufferFile))
+			log.Printf("count = {%d}, bufferFile = {%s}, EOF.", count, str)
+			break
+		}
+		if err != nil {
+			if err != io.EOF {
+				log.Fatalf("An Error occurred reading the file. file ={%s}", args[1])
+			}
+			break
+		}
+		_, err = os.Stdout.Write(bufferFile[:count])
+		if err != nil {
+			log.Fatalf("An Error occurred writing to Stdout. file ={%s}", args[1])
+		}
+	}
 }
 
-func getFile(filename string) (os.File, func(), error) {
+func getFile(filename string) (*os.File, func(), error) {
 
 	if filename == "" {
 		log.Fatalln("invalid filename")
@@ -34,4 +67,6 @@ func getFile(filename string) (os.File, func(), error) {
 			return
 		}
 	}
+
+	return file, closer, err
 }
